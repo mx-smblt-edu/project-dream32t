38a28059e6cfd3936cbfa34a3b700bba506c8020
mx-smblt-edu <mx.smblt.edu@gmail.com>
2025-11-12 17:02:36 +0200

    init project.

---
+++ src/main.py
@@ -0,0 +1,18 @@
+"""
+The entry point module of the application.
+
+This module contains the main entry point of the program, designed to initialize
+and execute the core functionality of the application. It ensures that the primary
+logic is invoked only when the module is run as the main script.
+"""
+
+
+def main() -> None:
+    """
+    The main entry point of the application that initializes and executes the program.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
